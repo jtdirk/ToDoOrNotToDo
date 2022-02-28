0262623f7c97d9ee2b8c838210fe3f5ed71fbd25
Dirk Schramm <dirk_schramm@web.de>
2022-02-28 09:28:43 +0100

    main.py vorhin vergessen

--- main.py
+++ main.py
@@ -2,10 +2,26 @@
 import os
 from pathlib import Path
 import sys
+from model import BaseModel
+import pytodotxt
 
 from PySide6.QtCore import QUrl
 from PySide6.QtGui import QGuiApplication
-from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtQml import QQmlApplicationEngine, qmlRegisterType
+
+projectList = []
+
+def openTodotxt():
+    todotxt = pytodotxt.TodoTxt('todo.txt')
+    todotxt.parse()
+
+    for task in todotxt.tasks:
+        if not task.is_completed:
+            print(task.projects)
+            print("-----")
+
+def setup():
+    openTodotxt()
 
 CURRENT_DIRECTORY = Path(__file__).resolve().parent
 
@@ -13,10 +29,13 @@ if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
 
+    qmlRegisterType(BaseModel, "BaseModel", 1, 0, "BaseModel")
+
     filename = os.fspath(CURRENT_DIRECTORY / "main.qml")
     url = QUrl.fromLocalFile(filename)
 
-    engine.load(url)
-    if not engine.rootObjects():
-        sys.exit(-1)
-    sys.exit(app.exec())
+    setup()
+    #engine.load(url)
+    #if not engine.rootObjects():
+    #    sys.exit(-1)
+    #sys.exit(app.exec())
