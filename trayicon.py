9f2fb01e07f0a4818390ebcf683dbfe8294327da
Dirk Schramm <dirk_schramm@web.de>
2022-10-26 14:48:40 +0200

    Nochmal

--- trayicon.py
+++ trayicon.py
@@ -8,9 +8,6 @@ class SystemTrayIcon(QSystemTrayIcon):
     def __init__(self, parent=None):
         QSystemTrayIcon.__init__(self)
 
-        self.event_exit_click = None
-
-        self._mainwindowIsVisible = True
         self.icon = QIcon("Liste.svg")
         self.setIcon(self.icon)
 
@@ -22,32 +19,13 @@ class SystemTrayIcon(QSystemTrayIcon):
         self.activated.connect(self.on_icon_click)
         self.show()
 
-    def on_exit_click(self):
-        self.fire_exit_click()
-        exit(0)
+    exitClicked = Signal()
+    iconClicked = Signal()
 
-    def fire_exit_click(self):
-        if self.event_exit_click:
-            self.event_exit_click(self)
+    def on_exit_click(self):
+        self.exitClicked.emit()
+        exit(0)
 
     def on_icon_click(self, reason):
         if reason == self.Trigger:
-            if self._mainwindowIsVisible:
-                self._mainwindowIsVisible = False
-            else:
-                self._mainwindowIsVisible = True
-            
-            self.mainwindowIsVisible_changed.emit()
-
-    def _mainwindowIsVisible(self):
-        return self._mainwindowIsVisible
-
-    def setMainwindowIsVisible(self, value):
-        self._mainwindowIsVisible = value
-        self.mainwindowIsVisible_changed.emit()
-
-    @Signal
-    def mainwindowIsVisible_changed(self):
-        pass
-
-    mainwindowIsVisible = Property(bool, _mainwindowIsVisible, setMainwindowIsVisible, notify=mainwindowIsVisible_changed)
+            self.iconClicked.emit()
